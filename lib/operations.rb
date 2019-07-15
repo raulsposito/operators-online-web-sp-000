d4869a005b40623d33702df974186ae0ceabc5e9
Raul Sposito <raulsposito@gmail.com>
2019-07-15 00:30:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,14 +2,11 @@ require 'pry'
 def unsafe?(speed)
   if speed < 40 
     return TRUE
-    elsif speed > 60 
-    return TRUE
-  else
-    return FALSE
+    else speed > 60 
   end
 end
 
-#def not_safe?(speed)
-  #speed < 40 ? return TRUE : speed > 60 
-#end
-#end
+def not_safe?(speed)
+  speed < 40 ? return TRUE : speed > 60 
+end
+end
