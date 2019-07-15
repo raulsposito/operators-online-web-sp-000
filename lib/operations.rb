c0c82cc3bf380658e0e4e7fc6e5ac201d4cdc643
Raul Sposito <raulsposito@gmail.com>
2019-07-15 00:27:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed < 40 
+    return TRUE
+    else speed > 60 
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+  speed < 40 ? return TRUE : speed > 60 
 end
-	
-
-
+end
